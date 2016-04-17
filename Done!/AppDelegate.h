d27681ea2d6cc3bde079708c18841e6d66ba9a5f
NeilDeveloper <neil@nspiresoftware.net>
2016-04-17 13:33:43 -0400

    Initial Commit

---
+++ Done!/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Done!
+//
+//  Created by Yongyang Nie on 4/17/16.
+//  Copyright © 2016 Yongyang Nie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Done!/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Done!
+//
+//  Created by Yongyang Nie on 4/17/16.
+//  Copyright © 2016 Yongyang Nie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
